5e88f9e3316f61e211ba0345e13b2b23f454e082
Nayru <nanualexandraramirez@gmail.com>
2022-07-22 12:03:44 -0500

    adding field link

--- portfolio/models.py
+++ portfolio/models.py
@@ -6,4 +6,5 @@ class Project(models.Model):
     name = models.CharField(max_length=50)
     description = models.CharField(max_length=200)
     github = models.CharField(max_length=300)
+    link = models.ChardField(max_length=300)
     slug = models.SlugField(unique=True)
