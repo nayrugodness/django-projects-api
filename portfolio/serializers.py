e93a9f3460be6e8cc718ea4505ba9678be8d53de
Nayru <nanualexandraramirez@gmail.com>
2022-07-22 16:14:56 -0500

    creating framework serializer

--- portfolio/serializers.py
+++ portfolio/serializers.py
@@ -11,3 +11,8 @@ class LanguageSerializer(serializers.ModelSerializer):
     class Meta:
         model = Language
         fields = '__all__'
+
+class FrameworkSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Framework
+        fields = '__all__'
