8822878736b40e54d9f5f2c7de63ba1d2685ffab
Nayru <nanualexandraramirez@gmail.com>
2022-07-22 15:58:14 -0500

    adding '' urls

--- personal_portfolio/urls.py
+++ personal_portfolio/urls.py
@@ -19,5 +19,8 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
+    path('', include('portfolio.urls')),
     path('admin/', admin.site.urls),
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('', include('pwa.urls')),
 ]
