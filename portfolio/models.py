d4dd393012ec7b53419985bf6a9b54fb57617015
Nayru <nanualexandraramirez@gmail.com>
2022-07-22 12:07:48 -0500

    creating language options

--- portfolio/models.py
+++ portfolio/models.py
@@ -1,6 +1,9 @@
 from django.db import models
 
-# Create your models here.
+Language=(
+
+)
+
 class Project(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=50)
