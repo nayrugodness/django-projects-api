5f8b38fbdd7525a886bde6297e44f3aa3168a8de
Nayru <nanualexandraramirez@gmail.com>
2022-07-22 16:13:42 -0500

    creating project serializer

--- portfolio/serializers.py
+++ portfolio/serializers.py
@@ -1,3 +1,8 @@
 from django.db.models.query import QuerySet
 from .models import Project, Language, Framework
 from rest_framework import fields, serializers
+
+class ProjectSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Project
+        fields = '__all__'
