2f2dccb9359c7914a4d7e74f56d78b09fea6cdc6
Nayru <nanualexandraramirez@gmail.com>
2022-07-22 15:32:45 -0500

    applying migrate

--- portfolio/models.py
+++ portfolio/models.py
@@ -2,14 +2,14 @@ from django.db import models
 
 class Language(models.Model):
     id = models.AutoField(primary_key=True)
-    name = models.ChardField(max_length=50)
+    name = models.CharField(max_length=50)
 
     def __str__(self):
         return self.name
 
 class Framework(models.Model):
     id = models.AutoField(primary_key=True)
-    name = models.ChardField(max_length=50)
+    name = models.CharField(max_length=50)
 
     def __str__(self):
         return self.name
@@ -19,7 +19,7 @@ class Project(models.Model):
     name = models.CharField(max_length=50)
     description = models.CharField(max_length=200)
     github = models.CharField(max_length=300)
-    link = models.ChardField(max_length=300)
+    link = models.CharField(max_length=300)
     language = models.ForeignKey(Language, on_delete=models.PROTECT)
     framework = models.ForeignKey(Framework, on_delete=models.PROTECT)
     slug = models.SlugField(unique=True)
