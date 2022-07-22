7c606ffc8c87ad1a6592539d1649c91fe1256798
Nayru <nanualexandraramirez@gmail.com>
2022-07-22 12:14:09 -0500

    creating model framework

--- portfolio/models.py
+++ portfolio/models.py
@@ -7,6 +7,13 @@ class Language(models.Model):
     def __str__(self):
         return self.name
 
+class Framework(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.ChardField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
 class Project(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=50)
