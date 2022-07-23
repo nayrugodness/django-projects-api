9ad0543b3f5121d3bdc94033e73d353dc3522838
Nayru <nanualexandraramirez@gmail.com>
2022-07-23 18:42:21 -0500

    deleting databases configuration

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -79,12 +79,7 @@ WSGI_APPLICATION = 'personal_portfolio.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+DATABASES = {}
 
 
 # Password validation
