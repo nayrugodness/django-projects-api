62c3c815dc8d1cdf6985b8cac973f953f28b3523
Nayru <nanualexandraramirez@gmail.com>
2022-07-23 09:45:15 -0500

    creating class project admin and addning that class in admin site register

--- portfolio/admin.py
+++ portfolio/admin.py
@@ -2,6 +2,10 @@ from django.contrib import admin
 from .models import Project, Language, Framework
 from .forms import ProjectForms, LanguageForms, FrameworkForms
 
-admin.site.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+
+    form = ProjectForms
+
+admin.site.register(Project, ProjectAdmin)
 admin.site.register(Language)
 admin.site.register(Framework)
