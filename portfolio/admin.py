32d669d09b3206ff703b12387b103004ff6ea7ee
Nayru <nanualexandraramirez@gmail.com>
2022-07-23 09:44:04 -0500

    importing forms in admin panel

--- portfolio/admin.py
+++ portfolio/admin.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
 from .models import Project, Language, Framework
-
-# Register your models here.
+from .forms import ProjectForms, LanguageForms, FrameworkForms
 
 admin.site.register(Project)
 admin.site.register(Language)
