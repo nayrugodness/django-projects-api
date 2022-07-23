4306bd479fe959dfde363dd3aa5aba38cb6ccf1b
Nayru <nanualexandraramirez@gmail.com>
2022-07-23 18:41:49 -0500

    allowing vercel host

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-env.eba-qhfiwnf7.us-west-2.elasticbeanstalk.com'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['django-env.eba-qhfiwnf7.us-west-2.elasticbeanstalk.com']
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
