e55d0ffec41d44e1637fe15c06a1267e5aa07c9b
Nayru <nanualexandraramirez@gmail.com>
2022-07-23 09:40:47 -0500

    creating class project forms

--- portfolio/forms.py
+++ portfolio/forms.py
@@ -1,2 +1,10 @@
 from django import forms
 from .models import Project, Language, Framework
+
+
+class ProjectForms(forms.ModelForm):
+
+    class Meta:
+
+        model = Project
+        fields = '__all__'
