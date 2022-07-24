fae01f611996fd74170630826e80583d37e09383
Nayru <nanualexandraramirez@gmail.com>
2022-07-24 13:24:51 -0500

    creating project router

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path, include
 from rest_framework import routers
+from .views import ProjectsViewset
 
 router = routers.DefaultRouter()
+router.register('projects', ProjectsViewset)
 
 urlpatterns = [
     path('api/', include(router.urls)),
