34fc700a3a6a5df32b26fafb82103e5d22cddd2f
Nayru <nanualexandraramirez@gmail.com>
2022-07-24 13:23:32 -0500

    creating project viewset]

--- portfolio/views.py
+++ portfolio/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from .models import Project
+from rest_framework import viewsets
+from .serializers import ProjectSerializer
 
 # Create your views here.
+
+class ProjectsViewset(viewsets.ModelViewSet):
+    queryset = Project.objects.all()
+    serializer_class = ProjectSerializer
+
+    def get_queryset(self):
+
+        projects = Project.objects.all()
+
+        name = self.request.GET.get('name')
+
+        if name:
+
+            projects = projects.filter(nombre__contains=name)
+
+        else:
+
+            return projects
