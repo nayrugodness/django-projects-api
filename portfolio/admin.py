710283e704d550cfaa42120a5715316125d35cdb
Nayru <nanualexandraramirez@gmail.com>
2022-07-24 12:23:31 -0500

    adding library to admin panel

--- portfolio/admin.py
+++ portfolio/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
-from .models import Project, Language, Framework
-from .forms import ProjectForms, LanguageForms, FrameworkForms
+from .models import Project, Language, Framework, Library
+from .forms import ProjectForms, LanguageForms, FrameworkForms, LibraryForms
 
 class ProjectAdmin(admin.ModelAdmin):
 
@@ -14,6 +14,11 @@ class FrameworkAdmin(admin.ModelAdmin):
 
     form = FrameworkForms
 
+class LibraryAdmin(admin.ModelAdmin):
+
+    form = LibraryForms
+
 admin.site.register(Project, ProjectAdmin)
 admin.site.register(Language, LanguageAdmin)
 admin.site.register(Framework, FrameworkAdmin)
+admin.site.register(Library, LibraryAdmin)
