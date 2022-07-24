c185ad4b8f7005ba4053d6397a5addc01fb872e7
Nayru <nanualexandraramirez@gmail.com>
2022-07-24 11:54:44 -0500

    adding database config

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -79,7 +79,12 @@ WSGI_APPLICATION = 'personal_portfolio.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-DATABASES = {}
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 
 # Password validation
