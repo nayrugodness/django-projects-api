05349f6e4aa36a6312518b52635619073e4bf58f
Nayru <nanualexandraramirez@gmail.com>
2022-07-24 12:02:26 -0500

    creating library model

--- portfolio/models.py
+++ portfolio/models.py
@@ -14,6 +14,14 @@ class Framework(models.Model):
     def __str__(self):
         return self.name
 
+
+class Library(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
 class Project(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=50)
